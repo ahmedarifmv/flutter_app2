a53b8eddfda8f5ee7e0087d37129ee02ddd8fef3
ahmed arif <ahmedarifmv@gmail.com>
2021-02-17 16:18:53 +0500

    initial version

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// 1) Create a new Flutter App (in this project) and output an AppBar and some text
+// below it
+// 2) Add a button which changes the text (to any other text of your choice)
+// 3) Split the app into three widgets: App, TextControl & Text
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: 
+        Text('hello every!!'),
+      
+    );
+  }
+}
